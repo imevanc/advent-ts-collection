77e43e50d2a19d59a892d12c0af705381c48a43e
imevanc <charal.vag@gmail.com>
2024-12-08 00:09:17 +0000

    feat: AOT: add day 7

---
+++ src/day7/validator.test.ts
@@ -0,0 +1,20 @@
+import type {Equal, Expect} from 'type-testing';
+import {createRoute} from "./index";
+
+const oneMill = createRoute('💨Dasher', ['Atherton', "Scarsdale", "Cherry Hills Village"]).route;
+type t0_actual = typeof oneMill; // =>
+type t0_expected = [           // =>
+    'Atherton',
+    "Scarsdale",
+    "Cherry Hills Village"
+];
+type t0 = Expect<Equal<t0_actual, t0_expected>>;
+
+const two = createRoute('🌟Vixen', ['Detroit', "Cleveland", "Dayton"]).route;
+type t1_actual = typeof two; // =>
+type t1_expected = [       // =>
+    'Detroit',
+    "Cleveland",
+    "Dayton"
+];
+type t1 = Expect<Equal<t1_actual, t1_expected>>;
