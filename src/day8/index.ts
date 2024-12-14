9af65ee3605cca17e4b0a63666b8463e4c0540b8
imevanc <charal.vag@gmail.com>
2024-12-14 21:34:44 +0000

    feat: AOT: add day 9

--- src/day8/index.ts
+++ src/day8/index.ts
@@ -1,22 +1,10 @@
-declare global {
-    namespace NodeJS {
-        interface ProcessEnv {
-                MOOD_LIGHTS: "true";
-                BATH_TEMPERATURE: "327.59";
-                STRAWBERRIES: "chocolate";
-        }
+declare module "santas-special-list" {
+    export type Status = "naughty" | "nice";
+    export interface Child {
+        name: string;
+        status: Status;
     }
+    export type List = Child[];
 }
 
-const { MOOD_LIGHTS } = process.env;
-export type t0_actual = typeof MOOD_LIGHTS;
-export type t0_expected = 'true';
 
-
-const { BATH_TEMPERATURE } = process.env;
-export type t1_actual = typeof BATH_TEMPERATURE;
-export type t1_expected = '327.59';
-
-const { STRAWBERRIES } = process.env;
-export type t2_actual = typeof STRAWBERRIES;
-export type t2_expected = 'chocolate';
--- src/day8/validator.test.ts
+++ src/day8/validator.test.ts
@@ -1,5 +1,18 @@
-import type {Equal, Expect} from 'type-testing';
-import {t0_actual, t0_expected, t1_actual, t1_expected, t2_actual, t2_expected} from "./index";
+import type {Equal, Expect} from "type-testing";
+import type {Child, List, Status} from "santas-special-list";
+
+type t0_actual = Status;
+type t0_expected = 'naughty' | 'nice';
+
+
+type t1_actual = Child;
+type t1_expected = {
+    name: string,
+    status: Status
+};
+
+type t2_actual = List;
+type t2_expected = Child[];
 
 type t0 = Expect<Equal<t0_actual, t0_expected>>;
 type t1 = Expect<Equal<t1_actual, t1_expected>>;
